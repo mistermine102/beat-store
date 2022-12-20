68540b7eef707e047ecafa985973ea5487b4d829
mistermine102 <66329106+mistermine102@users.noreply.github.com>
2022-12-20 22:21:06 +0100

    basic footer

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -3,3 +3,17 @@
         <h1>Footer</h1>
     </footer>
 </template>
+
+<style scoped>
+
+    footer {
+        background-color: rgb(15, 15, 30);
+        position: relative;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        padding: 2rem;
+        margin-top: auto;
+    }
+
+</style>
