453503e9fe4aa2bbcfa6c0b9450e0ec602da2569
keypervlad111 <keypervlad@gmail.com>
2023-01-17 16:53:53 +0100

    Style Login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -3,12 +3,12 @@
     <div class="col-6 offset-3">
       <h1>Login Page</h1>
       <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
+        <label for="exampleInputEmail1">Email address:</label>
         <input v-model.trim="emailInput" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
       <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
+        <label for="exampleInputPassword1">Password:</label>
         <input v-model.trim="passwordInput" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
       </div>
       <div class="form-check">
@@ -16,7 +16,7 @@
         <label class="form-check-label" for="exampleCheck1">Check me out</label>
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
-      <router-link to="/register">Register</router-link>
+      <router-link class="btn btn-secondary" to="/register">Register</router-link>
     </div>
   </form>
 </template>
@@ -39,3 +39,30 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.btn-primary{
+    background-color: brown !important;
+    border: none;
+    margin-top: 1rem;
+}
+.btn-primary:hover{
+  background-color: rgb(184, 63, 63);
+  transform: scale(1.05);
+  transition: .5s;
+}
+.btn-secondary{
+  margin-top: 1rem;
+  border: none;
+  background: none;
+  /* color: rgb(200, 0, 0); */
+}
+.btn-secondary:hover{
+  /* color: rgb(255, 0, 0); */
+  transform: scale(1.10);
+  transition: .5s;
+}
+.form-group{
+  margin-top: 1.5rem;
+}
+</style>
