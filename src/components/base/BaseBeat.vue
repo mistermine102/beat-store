1b696910139645df353ade2770cdf4798cd2fa8e
Szymon <szymonjarosz102@gmail.com>
2023-01-19 20:44:06 +0100

    styling base beat cmp

--- src/components/base/BaseBeat.vue
+++ src/components/base/BaseBeat.vue
@@ -3,16 +3,21 @@
     <audio ref="audio" :src="url"></audio>
     <div class="controls">
       <button class="btn btn-success" @click="play">Play</button>
-      <button class="btn btn-danger" @click="pause">Pause</button>
-      <!-- <progress @click="setCurrentTime" :value="progress" max="100"></progress> -->
-      <div class="progress-container">
-        <div class="progress position-relative">
-          <input id="progress-input" class="form-range position-absolute" @input="changeCurrentTime" type="range" v-model="progress" />
-          <div class="progress-bar position-relative" role="progressbar" aria-label="Basic example" :style="{ width: `${progress}%` }" :aria-valuenow="progress" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
-      </div>
+      <input
+        ref="progressBar"
+        type="range"
+        min="0"
+        max="100"
+        @input="changeCurrentTime"
+        v-model="progress"
+      />
       <button @click="mute">Mute</button>
-      <input class="form-range" v-model="volume" @input="changeVolume" type="range" />
+      <input
+        type="range"
+        v-model="volume"
+        @input="changeVolume"
+        class="win10-thumb volume"
+      />
     </div>
   </div>
 </template>
@@ -90,30 +95,173 @@ export default {
   },
   mounted() {
     this.$refs.audio.volume = 0.25;
+    console.log(this.$refs.progressBar);
   },
 };
 </script>
 <style scoped>
-.progress {
-  height: 10px;
-}
-.progress-bar {
-  transition: none;
+.controls {
+  display: flex;
 }
 
-.progress-input {
-  height: 10px;
+.volume {
+  transform: scale(0.5) translateX(-50%);
 }
 
-.progress-input::-webkit-slider-thumb {
-  box-shadow: 0px 0px 0px #000000;
-  border: 0px solid #000000;
-  height: 20px;
-  width: 39px;
-  border-radius: 7px;
-  background: #65001c;
-  cursor: pointer;
+input[type="range"] {
+  font-size: 1.5rem;
+  width: 12.5em;
+}
+
+input[type="range"] {
+  color: #ef233c;
+  --thumb-height: 0.5em;
+  --track-height: 0.125em;
+  --track-color: rgba(255, 255, 255, 0.8);
+  --brightness-hover: 120%;
+  --brightness-down: 95%;
+  --clip-edges: 0.125em;
+}
+
+input[type="range"].win10-thumb {
+  color: rgb(135, 135, 135);
+  --thumb-height: 1.375em;
+  --thumb-width: 0.5em;
+  --clip-edges: 0.0125em;
+}
+
+/* === range commons === */
+input[type="range"] {
+  position: relative;
+  background: #fff0;
+  overflow: hidden;
+}
+
+input[type="range"]:disabled {
+  filter: grayscale(1);
+  opacity: 0.3;
+  cursor: not-allowed;
+}
+
+/* === WebKit specific styles === */
+input[type="range"],
+input[type="range"]::-webkit-slider-runnable-track,
+input[type="range"]::-webkit-slider-thumb {
   -webkit-appearance: none;
-  margin-top: -3.5px;
+  transition: all ease 100ms;
+  height: var(--thumb-height);
+}
+
+input[type="range"]::-webkit-slider-runnable-track,
+input[type="range"]::-webkit-slider-thumb {
+  position: relative;
+}
+
+input[type="range"]::-webkit-slider-thumb {
+  --thumb-radius: calc((var(--thumb-height) * 0.5) - 1px);
+  --clip-top: calc((var(--thumb-height) - var(--track-height)) * 0.5 - 0.5px);
+  --clip-bottom: calc(var(--thumb-height) - var(--clip-top));
+  --clip-further: calc(100% + 1px);
+  --box-fill: calc(-100vmax - var(--thumb-width, var(--thumb-height))) 0 0
+    100vmax currentColor;
+
+  width: var(--thumb-width, var(--thumb-height));
+  background: linear-gradient(currentColor 0 0) scroll no-repeat left center /
+    50% calc(var(--track-height) + 1px);
+  background-color: currentColor;
+  box-shadow: var(--box-fill);
+  border-radius: var(--thumb-width, var(--thumb-height));
+
+  filter: brightness(100%);
+  clip-path: polygon(
+    100% -1px,
+    var(--clip-edges) -1px,
+    0 var(--clip-top),
+    -100vmax var(--clip-top),
+    -100vmax var(--clip-bottom),
+    0 var(--clip-bottom),
+    var(--clip-edges) 100%,
+    var(--clip-further) var(--clip-further)
+  );
+}
+
+input[type="range"]:hover::-webkit-slider-thumb {
+  filter: brightness(var(--brightness-hover));
+}
+
+input[type="range"]:active::-webkit-slider-thumb {
+  filter: brightness(var(--brightness-down));
+}
+
+input[type="range"]::-webkit-slider-runnable-track {
+  background: linear-gradient(var(--track-color) 0 0) scroll no-repeat center /
+    100% calc(var(--track-height) + 1px);
+}
+
+input[type="range"]:disabled::-webkit-slider-thumb {
+  cursor: not-allowed;
+}
+
+/* === Firefox specific styles === */
+input[type="range"],
+input[type="range"]::-moz-range-track,
+input[type="range"]::-moz-range-thumb {
+  appearance: none;
+  transition: all ease 100ms;
+  height: var(--thumb-height);
+}
+
+input[type="range"]::-moz-range-track,
+input[type="range"]::-moz-range-thumb,
+input[type="range"]::-moz-range-progress {
+  background: #fff0;
+}
+
+input[type="range"]::-moz-range-thumb {
+  background: currentColor;
+  border: 0;
+  width: var(--thumb-width, var(--thumb-height));
+  border-radius: var(--thumb-width, var(--thumb-height));
+  cursor: grab;
+}
+
+input[type="range"]:active::-moz-range-thumb {
+  cursor: grabbing;
+}
+
+input[type="range"]::-moz-range-track {
+  width: 100%;
+  background: var(--track-color);
+}
+
+input[type="range"]::-moz-range-progress {
+  appearance: none;
+  background: currentColor;
+  transition-delay: 30ms;
+}
+
+input[type="range"]::-moz-range-track,
+input[type="range"]::-moz-range-progress {
+  height: calc(var(--track-height) + 1px);
+  border-radius: var(--track-height);
+}
+
+input[type="range"]::-moz-range-thumb,
+input[type="range"]::-moz-range-progress {
+  filter: brightness(100%);
+}
+
+input[type="range"]:hover::-moz-range-thumb,
+input[type="range"]:hover::-moz-range-progress {
+  filter: brightness(var(--brightness-hover));
+}
+
+input[type="range"]:active::-moz-range-thumb,
+input[type="range"]:active::-moz-range-progress {
+  filter: brightness(var(--brightness-down));
+}
+
+input[type="range"]:disabled::-moz-range-thumb {
+  cursor: not-allowed;
 }
 </style>
